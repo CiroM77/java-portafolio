beeb4d6927a7baf3114e85f52c0b7c738bdce0d9
Ciro77 <ciro.pato99@gmail.com>
2022-07-31 22:58:09 -0300

    Backend Spring boot

--- src/main/java/com/java/portafolio/Controllers/EducacionController.java
+++ src/main/java/com/java/portafolio/Controllers/EducacionController.java
@@ -8,6 +8,7 @@ import com.java.portafolio.Service.IeducacionService;
 import com.java.portafolio.models.Educacion;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@CrossOrigin("*")
 public class EducacionController {
     @Autowired
     private IeducacionService eduService;
--- src/main/java/com/java/portafolio/Controllers/PersonaController.java
+++ src/main/java/com/java/portafolio/Controllers/PersonaController.java
@@ -8,6 +8,7 @@ import com.java.portafolio.Service.IpersonaService;
 import com.java.portafolio.models.Persona;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 public class PersonaController {
     @Autowired
     private IpersonaService persoService;
